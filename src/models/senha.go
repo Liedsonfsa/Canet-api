50a3472a605bf35c4fc0c7300efc1500e4b8f7fd
Liedsonfsa <fabricoliedson@gmail.com>
2024-09-15 14:46:08 -0300

    style: não lembro o que eu fiz

--- src/models/senha.go
+++ src/models/senha.go
@@ -2,6 +2,6 @@ package models
 
 // Senha armazena as senhas nova e atual na hora da mudança de senha
 type Senha struct {
-	Nova string `json:"nova"`
-	Atual string `json:"atual"`
+	Nova 	string `json:"nova"`
+	Atual 	string `json:"atual"`
 }
